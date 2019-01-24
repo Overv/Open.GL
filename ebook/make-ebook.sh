63b4d7714d7ed68807386b143c1018b2a9bfce71
Alexander Overvoorde <overv161@gmail.com>
2019-01-24 21:36:16 +0100

    Add build script for epub version

--- ebook/make-ebook.sh
+++ ebook/make-ebook.sh
@@ -16,3 +16,21 @@ pandoc \
     --indented-code-classes=cpp \
     --toc \
     -o "ebook/Modern OpenGL Guide.pdf"
+
+pandoc \
+    -s \
+    ebook/title.md \
+    content/articles-en/introduction.md \
+    content/articles-en/context.md \
+    content/articles-en/drawing.md \
+    content/articles-en/textures.md \
+    content/articles-en/transformations.md \
+    content/articles-en/depthstencils.md \
+    content/articles-en/framebuffers.md \
+    content/articles-en/geometry.md \
+    content/articles-en/feedback.md \
+    --highlight-style=zenburn \
+    --indented-code-classes=cpp \
+    --toc \
+    --webtex="https://latex.codecogs.com/png.latex?" \
+    -o "ebook/Modern OpenGL Guide.epub"
